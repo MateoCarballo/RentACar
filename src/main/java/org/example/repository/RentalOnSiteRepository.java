32d6c711f6d46c14a0b6f436ee220330a2dfbda8
Mateo Carballo Alonso <a22mateoca@ATEIS.LOCAL>
2023-04-24 17:45:16 +0200

    .

--- src/main/java/org/example/repository/RentalOnSiteRepository.java
+++ src/main/java/org/example/repository/RentalOnSiteRepository.java
@@ -1,4 +1,32 @@
 package org.example.repository;
 
-public class RentalOnSiteRepository {
+import org.example.model.Car.Car;
+import org.example.model.Client;
+import org.example.model.RentalOffice;
+
+public class RentalOnSiteRepository implements IRentalRepository{
+    @Override
+    public void isFor() {
+
+    }
+
+    @Override
+    public RentalOffice searchOffice(String address) {
+        return null;
+    }
+
+    @Override
+    public Client searchClient(int clientId) {
+        return null;
+    }
+
+    @Override
+    public Car searchCar(int carId) {
+        return null;
+    }
+
+    @Override
+    public void makeRent(Client customer, Car car, RentalOffice rentalOffice) {
+
+    }
 }
--- src/main/java/org/example/repository/WebRentalRepository.java
+++ src/main/java/org/example/repository/WebRentalRepository.java
@@ -2,7 +2,32 @@ package org.example.repository;
 
 import org.example.model.Car.Car;
 import org.example.model.Client;
+import org.example.model.RentalOffice;
 
-public class WebRentalRepository implements IWebRentalRepository {
+public class WebRentalRepository implements IRentalRepository {
 
+    @Override
+    public void isFor() {
+        
+    }
+
+    @Override
+    public RentalOffice searchOffice(String address) {
+        return null;
+    }
+
+    @Override
+    public Client searchClient(int clientId) {
+        return null;
+    }
+
+    @Override
+    public Car searchCar(int carId) {
+        return null;
+    }
+
+    @Override
+    public void makeRent(Client customer, Car car, RentalOffice rentalOffice) {
+
+    }
 }
